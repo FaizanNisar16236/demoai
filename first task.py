dee81aa3a753e2895f506c47793f12372e6af34c
FaizanNisar16236 <faizan623607@gmail.com>
2024-07-18 16:54:26 +0500

    first python assigniment

---
+++ first task.py
@@ -0,0 +1,13 @@
+# ls = [1, 2,3,4]
+# print(ls)
+
+# for y in ls:
+#     print(y)
+
+ls = ["Islamabad","Lahore","Karachi","Rawalpindi","Fasilabad"]
+user =input("enter city:  ")
+
+if(user in ls):
+    print("cleanest")
+else:
+    print("dirty")
